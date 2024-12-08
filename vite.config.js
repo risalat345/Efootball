f22e0abbec86fa540bae7d3b38fba8c6eeb2bd01
risalat345 <ashrafuddinbbhuiyanrisalat>
2024-12-08 21:20:04 +0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'; // Ensure this is at the top
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Efootball',
+});
